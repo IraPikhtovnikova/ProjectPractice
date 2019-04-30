171da1152c3d92c8bd04e5d3b2ddb5faccb7c4f0
IraPikhtovnikova <ira.pikht@mail.ru>
2019-04-30 14:12:29 +0500

    30/04/ Добавлена вся л.р.2

--- pp2/lab_2.sql
+++ pp2/lab_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 26 2019 г., 14:24
+-- Время создания: Апр 30 2019 г., 12:07
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.6.32
 
@@ -47,11 +47,36 @@ DELETE FROM client WHERE id_client = id$$
 CREATE DEFINER=`root`@`%` PROCEDURE `client_update` (IN `name` VARCHAR(100), IN `id` INT)  NO SQL
 UPDATE client SET client_name = name WHERE id_client = id$$
 
+CREATE DEFINER=`root`@`%` PROCEDURE `requests_services_add` (IN `request` INT, IN `service` INT)  NO SQL
+INSERT INTO `request_service` (request_id, service_id) VALUES (request, service)$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `requests_services_all` ()  NO SQL
+SELECT `request_service`.`id`, `request`.`id_request`, `request`.`client_id`, `client`.`client_name`, `service`.`service_name`, `service`.`service_price` FROM `client`, `request`, `service`, `request_service`
+WHERE `client`.`id_client` = `request`.`client_id` 
+AND `service`.`id_service` = `request_service`.`service_id` 
+AND `request`.`id_request` = `request_service`.`request_id`$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `requests_services_by_id` (IN `id` INT)  NO SQL
+SELECT `request_service`.`id`, `request`.`id_request`, `request`.`client_id`, `client`.`client_name`, `service`.`service_name`, `service`.`service_price` FROM `client`, `request`, `service`, `request_service`
+WHERE `client`.`id_client` = `request`.`client_id` 
+AND `service`.`id_service` = `request_service`.`service_id` 
+AND `request`.`id_request` = `request_service`.`request_id`
+AND `request_service`.`id` = id$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `requests_services_delete` (IN `id` INT)  NO SQL
+DELETE FROM `request_service` WHERE `request_service`.`id` = id$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `requests_services_update` (IN `request` INT, IN `service` INT, IN `id` INT)  NO SQL
+UPDATE `request_service` SET request_id = request, service_id = service WHERE `request_service`.`id` = id$$
+
 CREATE DEFINER=`root`@`%` PROCEDURE `request_add` (IN `id_client` INT)  NO SQL
 INSERT INTO request (client_id) VALUES (id_client)$$
 
 CREATE DEFINER=`root`@`%` PROCEDURE `request_by_id` (IN `id` INT)  NO SQL
-SELECT id_request, id_client FROM client, request WHERE d_client = client_id AND id_request = id$$
+SELECT id_request, id_client, `client`.`client_name` FROM client, request WHERE id_client = client_id AND id_request = id$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `request_delete` (IN `id` INT)  NO SQL
+DELETE FROM `request` WHERE `request`.`id_request` = id$$
 
 CREATE DEFINER=`root`@`%` PROCEDURE `request_update` (IN `client` INT, IN `id` INT)  NO SQL
 UPDATE request SET client_id = client WHERE id_request = id$$
@@ -87,7 +112,8 @@ CREATE TABLE `client` (
 
 INSERT INTO `client` (`id_client`, `client_name`) VALUES
 (2, 'Иванов Петр Сергеевич'),
-(7, 'Кукушкина Валерия Сергеевна');
+(7, 'Кукушкина Валерия Сергеевна'),
+(8, 'Кошкин Петр Валентинович');
 
 -- --------------------------------------------------------
 
@@ -100,6 +126,14 @@ CREATE TABLE `request` (
   `client_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `request`
+--
+
+INSERT INTO `request` (`id_request`, `client_id`) VALUES
+(4, 8),
+(5, 8);
+
 -- --------------------------------------------------------
 
 --
@@ -107,9 +141,17 @@ CREATE TABLE `request` (
 --
 
 CREATE TABLE `request_service` (
+  `id` int(11) NOT NULL,
   `request_id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `request_service`
+--
+
+INSERT INTO `request_service` (`id`, `request_id`, `service_id`) VALUES
+(1, 4, 4);
 
 -- --------------------------------------------------------
 
@@ -128,7 +170,8 @@ CREATE TABLE `service` (
 --
 
 INSERT INTO `service` (`id_service`, `service_name`, `service_price`) VALUES
-(1, 'Прочитать книгу', 100);
+(3, 'Написать другу', 180),
+(4, 'Позвонить другу', 200);
 
 --
 -- Индексы сохранённых таблиц
@@ -153,6 +196,7 @@ ALTER TABLE `request`
 -- Индексы таблицы `request_service`
 --
 ALTER TABLE `request_service`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `request_id` (`request_id`),
   ADD KEY `service_id` (`service_id`);
 
@@ -171,19 +215,25 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT для таблицы `client`
 --
 ALTER TABLE `client`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `request`
 --
 ALTER TABLE `request`
-  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `request_service`
+--
+ALTER TABLE `request_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `service`
 --
 ALTER TABLE `service`
-  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
